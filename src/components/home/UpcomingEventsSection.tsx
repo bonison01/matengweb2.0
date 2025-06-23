4e6eb48d486c7ae6874a5072814c0da3a5c2e320
bonison01 <164063307+bonison01@users.noreply.github.com>
2025-06-24 00:01:40 +0530

    dsf

--- src/components/home/UpcomingEventsSection.tsx
+++ src/components/home/UpcomingEventsSection.tsx
@@ -73,7 +73,7 @@ const UpcomingEventsSection = () => {
                 </div> */}
 
                 <h3 className="text-2xl md:text-3xl font-bold mb-4">{featuredEvent.title}</h3>
-                {/* <p className="mb-6">{featuredEvent.description}</p> */}
+                <p className="mb-6">{featuredEvent.description}</p>
 
                 {/* <div className="flex items-center mb-6">
                   <Trophy className="w-5 h-5 mr-2" />
