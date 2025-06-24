46913b82464a79d373fc0e821ca8eadd85fa900b
bonison01 <164063307+bonison01@users.noreply.github.com>
2025-06-24 10:38:11 +0530

    dsf

--- src/pages/AnswerKeyPage.tsx
+++ src/pages/AnswerKeyPage.tsx
@@ -28,7 +28,7 @@ const AnswerKeyPage = () => {
                         <div className="bg-white p-6 rounded-lg shadow-md">
                             <h2 className="text-2xl font-semibold mb-4 text-primary">Class 5</h2>
                             <a
-                                href="https://drive.google.com/file/d/1G-BeuftbH4gRQVWESUKF4NNXTexxpnKf/view?usp=drive_link"
+                                href="https://drive.google.com/file/d/1pyt3wfkHLBIcNxMktSFkfOBRkSj_SpFz/view?usp=drive_link"
                                 target="_blank"
                                 rel="noopener noreferrer"
                                 className="inline-block bg-primary text-white font-medium px-5 py-3 rounded hover:bg-primary-dark transition"
@@ -46,6 +46,100 @@ const AnswerKeyPage = () => {
                                 className="inline-block bg-primary text-white font-medium px-5 py-3 rounded hover:bg-primary-dark transition"
                             >
                                 Download PDF
+                            </a>
+                        </div>
+                    </div><br /><br /><br /><br />
+
+                    {/* Question Papers */}
+                </div>
+                <div className="container mx-auto px-6 text-center"><br /><br />
+                    <h1 className="text-4xl font-bold mb-8">Questions Papers for all set.</h1>
+                    <p className="text-lg text-gray-600 mb-10">
+                        Download the PDF question papers for each class listed below.
+                    </p>
+
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-8 max-w-4xl mx-auto">
+                        <div className="bg-white p-6 rounded-lg shadow-md">
+                            <h2 className="text-2xl font-semibold mb-4 text-primary">Class 4</h2>
+                            <a
+                                href="https://drive.google.com/file/d/1buBCdU9EQycW3PElT2NKuIZIUtSgsbwE/view?usp=drive_link"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="inline-block bg-primary text-white font-medium px-5 py-3 rounded hover:bg-primary-dark transition"
+                            >
+                                SET A
+                            </a><br /><br /><br />
+                            <a
+                                href="https://drive.google.com/file/d/1iQ-yNW_yAuEqyqhO1UgL7s5_NnU9wQLZ/view?usp=drive_link"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="inline-block bg-primary text-white font-medium px-5 py-3 rounded hover:bg-primary-dark transition"
+                            >
+                                SET B
+                            </a><br /><br /><br />
+                            <a
+                                href="https://drive.google.com/file/d/1vYMjiM2aWAZIUtgg7DP94P__qpixaTP1/view?usp=drive_link"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="inline-block bg-primary text-white font-medium px-5 py-3 rounded hover:bg-primary-dark transition"
+                            >
+                                SET C
+                            </a>
+                        </div>
+
+                        <div className="bg-white p-6 rounded-lg shadow-md">
+                            <h2 className="text-2xl font-semibold mb-4 text-primary">Class 5</h2>
+                            <a
+                                href="https://drive.google.com/file/d/1DI98uPxy_Lumsw23C1ZLRTV_tvwP9jwT/view?usp=drive_link"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="inline-block bg-primary text-white font-medium px-5 py-3 rounded hover:bg-primary-dark transition"
+                            >
+                                SET A
+                            </a><br /><br /><br />
+                            <a
+                                href="https://drive.google.com/file/d/11gGNS3_vfzzc0nh5_r-LyEMx2FlqSlNE/view?usp=drive_link"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="inline-block bg-primary text-white font-medium px-5 py-3 rounded hover:bg-primary-dark transition"
+                            >
+                                SET B
+                            </a><br /><br /><br />
+                            <a
+                                href="https://drive.google.com/file/d/1wHRsoCz6FdEevBn-AiZJLFqZUF9u5QTT/view?usp=drive_link"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="inline-block bg-primary text-white font-medium px-5 py-3 rounded hover:bg-primary-dark transition"
+                            >
+                                SET C
+                            </a>
+                        </div>
+
+                        <div className="bg-white p-6 rounded-lg shadow-md">
+                            <h2 className="text-2xl font-semibold mb-4 text-primary">Class 6</h2>
+                            <a
+                                href="https://drive.google.com/file/d/18YJDqCr2mCfCM1LkoJ_2v-iyCjqk7Fcf/view?usp=drive_link"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="inline-block bg-primary text-white font-medium px-5 py-3 rounded hover:bg-primary-dark transition"
+                            >
+                                SET A
+                            </a><br /><br /><br />
+                            <a
+                                href="https://drive.google.com/file/d/1LqgwPfWV4F39xjKz19Nz7cP0htNkMk3R/view?usp=drive_link"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="inline-block bg-primary text-white font-medium px-5 py-3 rounded hover:bg-primary-dark transition"
+                            >
+                                SET B
+                            </a><br /><br /><br />
+                            <a
+                                href="https://drive.google.com/file/d/1Lj4Jfy77Yzym_5ApF-WHELgP86df3dri/view?usp=drive_link"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="inline-block bg-primary text-white font-medium px-5 py-3 rounded hover:bg-primary-dark transition"
+                            >
+                                SET C
                             </a>
                         </div>
                     </div>
